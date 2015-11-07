d58f4d67d23cf8a5504f8781f6633b80891385ef
Kilian Koeltzsch <me@kilian.io>
2015-11-07 11:09:08 +0100

    clean up and prepare for upcoming changes

--- buerostatus.ino
+++ buerostatus.ino
@@ -1,24 +1,13 @@
 int eingang = A0; // Lichtsensor ist am Analogport 0
-int LED = 10;
 int sensorWert = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode (LED, OUTPUT);
 }
 
 void loop() {
   sensorWert = analogRead(eingang);
-
-  Serial.print("Sensorwert = " );
   Serial.println(sensorWert);
-
-  // Alles ueber 100 scheint gut auf Licht im Buero hinzuweisen.
-  if (sensorWert > 100 ) {
-    digitalWrite(LED, HIGH);
-  } else {
-    digitalWrite(LED, LOW);
-  }
-
-  delay (50);
+  delay(50);
 }
+
---
+++ raspi.py
@@ -0,0 +1,9 @@
+import pyserial
+
+# Read the value from the serial output
+
+# Save value to local SQLite DB
+
+# Upload value to Kaki
+
+# Don't forget to run this script periodically (via cron every 10 minutes?)
