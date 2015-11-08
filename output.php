685228897c114b61a842f41651439941f4805047
Kilian Koeltzsch <me@kilian.io>
2015-11-08 21:53:50 +0100

    adjust output threshold

--- output.php
+++ output.php
@@ -3,7 +3,7 @@
 $db = new SQLite3("buerostatus.db");
 $result = $db->query("SELECT val FROM buerostatus ORDER BY id DESC LIMIT 1;");
 $val = $result->fetchArray()["val"];
-if ($val > 600) {
+if ($val > 250) {
 	echo 1;
 } else {
 	echo 0;
