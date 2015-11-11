4a30ab319f75d63c9cc2bad5b623ec1a3f9eedd9
dirko <dirkonet@users.noreply.github.com>
2015-11-11 22:52:51 +0100

    Median output, fixes #1
    lazy median implementation: assumes sample size to be odd, array size to be larger than half of the sample size

--- output.php
+++ output.php
@@ -1,12 +1,23 @@
 <?php
-
+$sampleSize = 5;
+$threshold = 250;
 $db = new SQLite3("buerostatus.db");
-$result = $db->query("SELECT val FROM buerostatus ORDER BY id DESC LIMIT 1;");
-$val = $result->fetchArray()["val"];
-if ($val > 250) {
-	echo 1;
-} else {
-	echo 0;
+$result = $db->query("SELECT val FROM buerostatus ORDER BY id DESC LIMIT $sampleSize;");
+$row = $result->fetchArray();
+
+$vals = [];
+while ($row != FALSE && count($row)>0) {
+        $vals[] = $row['val'];
+        $row = $result->fetchArray();
 }
 
+$medianElement = floor($sampleSize/2);
+sort($vals, SORT_NUMERIC);
+$val = $vals[$medianElement];
+
+if ($val > $threshold) {
+        echo 1;
+} else {
+        echo 0;
+}
 ?>
